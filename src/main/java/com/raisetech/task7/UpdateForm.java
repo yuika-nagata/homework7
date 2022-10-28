fab9cd4b89dc9661b675b3c5aa5e6a183f99f555
yuika nagata <xxx.yu.74@gmail.com>
2022-10-28 13:05:30 +0900

    バリデーションの実装

--- src/main/java/com/raisetech/task7/UpdateForm.java
+++ src/main/java/com/raisetech/task7/UpdateForm.java
@@ -1,7 +1,13 @@
 package com.raisetech.task7;
 
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+
 public class UpdateForm {
 
+    @NotBlank
+    @Length(max = 20)
     private String name;
     public String getName(){
         return name;
